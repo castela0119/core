bf0b365b7205e3615422d65e3141a9653824d04a
USER <castela0119@gmail.com>
2022-11-08 18:34:31 +0900

    5.6 @Configuration과 바이트코드 조작의 마법

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -8,11 +8,14 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+// @Configuration
 public class AppConfig {
+
+    @Autowired MemberRepository memberRepository;
 
     @Bean
     public MemberService memberService() {
